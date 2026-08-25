a331948d4e11aa52cf5db15339473245ccc1c061
brighterpractice <314649874+brighterpractice@users.noreply.github.com>
2026-08-25 15:59:04 -0600

    Initialize Brighter Sites marketing website

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://evergreen-counseling-demo.example',
+  output: 'static',
+  trailingSlash: 'always',
+});
